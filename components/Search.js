296652c1b7e29ce3bf9e45619a0b27cbceca8306
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-23 12:52:16 +0200

    Add send searchTerm

--- components/Search.js
+++ components/Search.js
@@ -10,6 +10,16 @@ Search = React.createClass({
     this.setState({
       searchTerm: searchTerm
     });
+
+    if (searchTerm.length > 2) {
+      this.props.onSearch(searchTerm);
+    }
+  },
+  
+  handleKeyUp: function(e) {
+    if (e.keyCode === 13) {
+      this.props.onSearch(this.state.searchTerm);
+    }
   },
 
   render: function() {
@@ -22,9 +32,10 @@ Search = React.createClass({
     return <input
       type='text'
       onChange={this.handleChange}
+      onKeyUp={this.onKeyUp}
       placeholder='What are you search for'
       style={styles}
       value={this.state.searchTerm}
       />
-  }
+  },
 })
