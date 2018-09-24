eeae08864bd7ec0e65c70a6097c42a27192d231e
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-24 16:32:01 +0200

    Fix typo

--- components/App.js
+++ components/App.js
@@ -49,7 +49,7 @@ App = React.createClass({
       <div style={styles}>
         <h1>GIF search engine</h1>
         <p>
-          Find GIF on <a href='http;//giphy.com'>giphy</a>
+          Find GIF on <a href='http://giphy.com'>giphy</a>
           To get next gif click Enter
         </p>
         <Search onSearch={this.handleSearch}/>
