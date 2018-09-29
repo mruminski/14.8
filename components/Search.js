7b37565a8bc3b3f25d4bcc94d45cfb005c3cbb29
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-29 08:31:26 +0200

    Add bracket to return in Search component

--- components/Search.js
+++ components/Search.js
@@ -29,7 +29,7 @@ Search = React.createClass({
       maxWidth: '350px'
     };
     
-    return
+    return (
       <input
         type='text'
         onChange={this.handleChange}
@@ -38,5 +38,6 @@ Search = React.createClass({
         style={styles}
         value={this.state.searchTerm}
       />
+  )
   },
 })
